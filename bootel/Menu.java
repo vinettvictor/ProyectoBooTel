e816492e6a45c6049ec0a3e0d76ed8c20ff4d47a
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-05-26 23:48:50 -0400

    Add files via upload

--- bootel/Menu.java
+++ bootel/Menu.java
@@ -5,107 +5,27 @@
  */
 package bootel;
 
-import archivo.Archivo;
 import java.util.Scanner;
+
 /**
  *
- * @author V. Vinett, C. Herrera, J. llanos.
+ * @author Vinett
  */
 public class Menu {
-    Scanner sc = new Scanner(System.in);
-    Archivo registra = new Archivo();
-    String nombreEstudiante,institucion,rutEstudiante;
-    int nDepto;
-    int precio=0;
-    int estado;
-   
-    private int opcion;
-    private boolean op = true;
+    Scanner scr = new Scanner(System.in);
+    MenuAdmin ma = new MenuAdmin();
+    MenuCliente mc = new MenuCliente();
     
     public void crearMenu(){
-        while(op){
+        
+        System.out.println("Ingrese una opcion (1. cliente , 2. admin)");
+        int opcion = scr.nextInt();
+        if (opcion == 1){
+            mc.crearMenuCliente();
             
-            System.out.println("Menu");
-            System.out.println("1. Mostrar departamentos disponibles");
-            System.out.println("2. Registrate");
-            System.out.println("3. Hacer una reserva");
-            System.out.println("4. Salir");
-            
-            System.out.println("Seleccione una opción");
-            opcion = sc.nextInt();
-            
-            if (opcion == 4){
-                
-                System.out.println("Usted ha salido del programa");
-                break;
-                
-            }else {
-                
-                switch(opcion){
-                    
-                    case 1:
-                        
-                        inventarioDepartamento depto = new inventarioDepartamento();
-                        depto.Depto();
-                        depto.mostrarDeptoDisponibles();
-                        break;
-                        
-                    case 2:
-                        Validador valida = new Validador();
-                        System.out.println("Bienvenido");
-                        do {
-                        System.out.println("Por favor ingrese su nombre ");
-                        nombreEstudiante = sc.next();
-                        if (!valida.validarNombre(nombreEstudiante)){
-                            System.out.println("Asegurese de estar ingresando un nombre correcto(La primera letra tiene que ser Mayuscula)");                            
-                        }
-                        }while(!(valida.validarNombre(nombreEstudiante)));
-                        
-                        do {     
-                        System.out.println("Por favor ingrese su rut (Con puntos y guion)");
-                        rutEstudiante  = sc.next();
-                        if(!valida.validarRut(rutEstudiante)){
-                            System.out.println("Asegurese de ingresar un rut correcto");                         
-                        }
-                        }while((!valida.validarRut(rutEstudiante))) ;
-                        
-                        do {
-                        System.out.println("Por favor ingrese su universidad a la que pertenece");
-                        institucion = sc.next();
-                        if(!valida.validarInstitucion(institucion)){
-                            System.out.println("Asegurate de ingresar una institucion correcta");
-                        }
-                        }while(!valida.validarInstitucion(institucion));                       
-                        Cliente estudiante = new Cliente(nombreEstudiante,rutEstudiante,institucion);
-                        registra.registrarUsuario(estudiante);
-                        break;
-                    case 3 :
-                        
-                        inventarioDepartamento p = new inventarioDepartamento();
-                        System.out.println("Para realizar una reserva asegurese de estar registrado");
-                        System.out.println("Ingrese su nombre");
-                        nombreEstudiante = sc.next();
-                        System.out.println("Ingrese su rut");
-                        rutEstudiante = sc.next();
-                        System.out.println("Ingrese el numero de departamento que desea");
-                        nDepto = sc.nextInt();
-                        
-                        precio = p.getValorDepto(nDepto);
-                        
-                        Reserva reserva = new Reserva(nombreEstudiante,rutEstudiante,nDepto,precio);  
-                        
-                        registra.reservarDepto(reserva,rutEstudiante,nombreEstudiante);
-                        
-                        
-                        break;
-                        
-                    default: 
-                        
-                        System.out.println("La opcion que ha marcado no es valida. por favor intente nuevamente");
-                    
-                        }
-                        
-            }
+        }else {
+            ma.crearMenuAdmin();
         }
+        
     }
 }
