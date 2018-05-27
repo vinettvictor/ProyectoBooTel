56bfcb20fe18e3d63a4f39294dd47182dea3e97d
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-05-27 18:19:54 -0400

    Add files via upload

---
+++ archivo/ArchivoRegister.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archivo;
+
+
+import bootel.Admin;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.*;
+import java.lang.String;
+import bootel.Reserva;
+import bootel.Cliente;
+import bootel.Register;
+/**
+ *
+ * @author Vinett
+ */
+public class ArchivoRegister {
+    
+    // Se escribira un archivo en la carpeta del proyecto llamado Reserva.txt
+    //Registo de Usuario
+     public void registrarUsuario(Cliente nueva){
+        try{
+            File f = new File("Registro.txt");
+            FileWriter fw;
+            BufferedWriter bw;
+           
+            if(f.exists()){
+                 fw = new FileWriter(f,true);
+                 bw = new BufferedWriter(fw);
+                 bw.newLine();
+                 bw.write(nueva.nombre+";"+nueva.rut+";"+nueva.pass+";"+nueva.institucion);
+              
+                
+            }else {
+                 fw = new FileWriter(f);
+                 bw = new BufferedWriter(fw);
+                 bw.write(nueva.nombre+";"+nueva.rut+";"+nueva.pass+";"+nueva.institucion);               
+            }
+            
+            bw.close();
+            fw.close();
+        }catch(Exception e){
+            System.out.println("ERROR");
+        }
+        
+    //Registro de Admin
+    //Se guarda en un archivo Admins.txt
+    }
+          public void registrarAdmin(Admin nueva){
+        try{
+            File f = new File("Admins.txt");
+            FileWriter fw;
+            BufferedWriter bw;
+           
+            if(f.exists()){
+                 fw = new FileWriter(f,true);
+                 bw = new BufferedWriter(fw);
+                 bw.newLine();
+                 bw.write(nueva.nombre+";"+nueva.rut+";"+nueva.pass+";"+nueva.codIdentificador);
+              
+                
+            }else {
+                 fw = new FileWriter(f);
+                 bw = new BufferedWriter(fw);
+                 bw.write(nueva.nombre+";"+nueva.rut+";"+nueva.pass+";"+nueva.codIdentificador);               
+            }
+            
+            bw.close();
+            fw.close();
+        }catch(Exception e){
+            System.out.println("ERROR");
+        }
+    
+    }
+}
