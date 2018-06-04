1afdbd868ae0f942c108e86ce39f84de4a8ceb02
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-06-04 13:13:17 -0400

    Add files via upload

--- bootel/BooTel.java
+++ bootel/BooTel.java
@@ -10,11 +10,8 @@ import gui.VentanaInicio;
  * @author Vinett
  */
 public class BooTel {
-    public static void main(String[] args) {
-        Menu m = new Menu();       
-        m.crearMenu();
-        
-        //VentanaInicio ventana = new VentanaInicio();
-        //ventana.setVisible(true);
+    public static void main(String[] args) {       
+        VentanaInicio ventana = new VentanaInicio();
+        ventana.setVisible(true);
     }
 }
