de62a619f5962336036a9eaf0baaba32ff2d4e40
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-06-30 22:06:44 -0400

    Add files via upload

--- bootel/Validador.java
+++ bootel/Validador.java
@@ -17,7 +17,7 @@ public   boolean validarRut (String rut ) {
         boolean validacion = false;
         
         try {
-            if(rut.matches("^[0-9]{1,2}.[0-9]{3}.[0-9]{3}-(k|[0-9]{1})$")){
+            if(rut.matches("^[0-9]{1,2}.[0-9]{3}.[0-9]{3}-(k|[0-9]{1})$")){ // exprecion regular para validar el rut 
                 validacion = true;
             }
         }catch(java.lang.NumberFormatException e){            
@@ -30,7 +30,7 @@ public boolean validarNombre(String nombre){
    
     boolean validacion = false;
     try {      
-        if (nombre.matches("^[A-Z]{1}[a-z]*$")){           
+        if (nombre.matches("^[A-Z]{1}[a-z]*$")){   // exprecion regular para validar nombre         
             validacion = true;
         }
                     
@@ -46,7 +46,7 @@ public boolean validarTexto(String institucion){
     boolean validacion = false;
     
     try {
-        if(institucion.matches("^[A-zZ-a]*$")){
+        if(institucion.matches("^[A-zZ-a]*$")){ // exprecion regular para validar textos 
             validacion = true;
         }
         
@@ -56,6 +56,20 @@ public boolean validarTexto(String institucion){
         
     }
     
+    return validacion;
+}
+
+public boolean validarPass (String pass) {
+    boolean validacion = false;
+    try {
+      
+        if (pass.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,15}[^'\\s]")) {
+            validacion = true;
+        }
+    }catch (java.lang.NumberFormatException e){
+        
+    }catch (Exception e){
+    }
     return validacion;
 }
 }
