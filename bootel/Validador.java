9117293240bc98d01d32fa044d9f6eed920d6feb
vinettvictor <32687602+vinettvictor@users.noreply.github.com>
2018-07-30 01:14:31 -0400

    Add files via upload

--- bootel/Validador.java
+++ bootel/Validador.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bootel;
-import java.util.regex.*;
+
 /**
  *
  * @author V. Vinett, C. Herrera, J. llanos.
  */
+
 public class Validador {
 
 //Metodo para validar rut
@@ -32,42 +28,32 @@ public boolean validarNombre(String nombre){
     try {      
         if (nombre.matches("^[A-Z]{1}[a-z]*$")){   // exprecion regular para validar nombre         
             validacion = true;
-        }
-                    
-    }catch(java.lang.NumberFormatException e) {
-        
-    }catch (Exception e){
-        
+        }                   
+    }catch(java.lang.NumberFormatException e) {        
+    }catch (Exception e){       
     }
    return validacion; 
 }
 
-public boolean validarTexto(String institucion){
-    boolean validacion = false;
-    
+public boolean validarTexto(String text){
+    boolean validacion = false;    
     try {
-        if(institucion.matches("^[A-zZ-a]*$")){ // exprecion regular para validar textos 
+        if(text.matches("^[A-zZ-a]*$")){ // exprecion regular para validar textos 
             validacion = true;
-        }
-        
-    }catch (java.lang.NumberFormatException e){
-        
-    }catch (Exception e){
-        
-    }
-    
+        }       
+    }catch (java.lang.NumberFormatException e){       
+    }catch (Exception e){       
+    }    
     return validacion;
 }
 
 public boolean validarPass (String pass) {
     boolean validacion = false;
-    try {
-      
+    try {      
         if (pass.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,15}[^'\\s]")) {
             validacion = true;
         }
-    }catch (java.lang.NumberFormatException e){
-        
+    }catch (java.lang.NumberFormatException e){       
     }catch (Exception e){
     }
     return validacion;
